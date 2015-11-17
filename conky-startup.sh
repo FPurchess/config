78d187b98dd196c8a6793feb33d450bca41cbd2b
Florian Purchess <florian.purchess@lovoo.com>
2015-11-17 14:46:17 +0100

    merged work + home

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,3 +1,5 @@
 killall conky
 cd "~/.conky/Conky Seamod"
 conky -c "~/.conky/Conky Seamod/conky_seamod" &
+cd "~/.conky/speedtracker/Slick_Conky"
+conky -c "~.conky/speedtracker/Slick_Conky/Slick_Conky" &
